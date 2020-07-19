9c67da4d4e53d00cdce53dce56b4425a1e86566d
Alexandre Amory <amamory@gmail.com>
2020-07-19 09:45:55 -0300

    simpler approach is working in behav sim. going for timing sim

--- hw/hdl/noc_counter.vhd
+++ hw/hdl/noc_counter.vhd
@@ -44,7 +44,7 @@ port (
 
 	-- axi master streaming interface
 	m_valid_o : out std_logic;
-	m_ready_i : in  std_logic; -- TODO, this port is not used and it will cause error when Noc has congestion
+	m_ready_i : in  std_logic;
 	m_last_o  : out std_logic;
 	m_data_o  : out std_logic_vector(31 downto 0)
 );
@@ -52,37 +52,15 @@ end noc_counter;
 
 architecture Behavioral of noc_counter is
 
-constant FIFO_SIZE : integer := 3; 
-
 -- incomming packet fsm
-type State_in_type IS (IDLE_IN, GET_SIZE, GET_STORE_SOURCE, WAIT_LAST_FLIT); 
-signal state_in : State_in_Type;    
-
--- outgoing packet fsm
-type State_out_type IS (IDLE_OUT, SEND_HEADER, SEND_SIZE, SEND_SOURCE, INCREMENTING); 
-signal state_out : State_out_type;    
-
--- fsm to get both in and out fsm in sync
-type State_handshake_type IS (WAIT_IN_PACKET, WAIT_OUT_PACKET); 
-signal state_handshake : State_handshake_type;    
-
-signal can_send      : std_logic;
+type State_type IS (WAIT_IN_HEADER, GET_SIZE, GET_SOURCE, SEND_HEADER, SEND_SIZE, SEND_SOURCE, INCREMENTING_PAYLOAD); 
+signal state : State_Type;    
 
 -- get the source of the incomming packet , which will be the target of the outgoing packet
 signal source_addr : std_logic_vector(15 downto 0);
 -- get the header of the incomming packet , which will be the source of the outgoing packet
 signal self_addr   : std_logic_vector(15 downto 0);
 signal size : std_logic_vector(15 downto 0);
-
-signal s_ready_s    : std_logic;
-
--- fifo signals
-signal fifo_wr      : std_logic;
-signal fifo_data_in : std_logic_vector(32 downto 0);
-signal fifo_full    : std_logic;
-signal fifo_rd      : std_logic;
-signal fifo_data_out: std_logic_vector(32 downto 0);
-signal fifo_empty   : std_logic;
 
 --attribute KEEP : string;
 --attribute MARK_DEBUG : string;
@@ -94,231 +72,81 @@ signal fifo_empty   : std_logic;
 
 begin
 
-    ----------------------------------------
-    -- INPUT PACKET PROCESSING
-    ----------------------------------------
-    u_fifo: entity work.fifo
-      generic map(
-        g_WIDTH => 33,
-        g_DEPTH => 8
-        )
-      port map(
-        i_rst_sync => reset_n,
-        i_clk      => clock,
-     
-        -- FIFO Write Interface
-        i_wr_en   => fifo_wr,
-        i_wr_data => fifo_data_in,
-        o_full    => fifo_full,
-     
-        -- FIFO Read Interface
-        i_rd_en   => fifo_rd,
-        o_rd_data => fifo_data_out,
-        o_empty   => fifo_empty
-        );
- 
-    -- write data from the master port into the fifo
---    process(clock, reset_n)
---    begin
---        if (reset_n = '0') then 
---            fifo_data_in <= (others => '0');
---            fifo_wr <= '0';
---        elsif (clock'event and clock = '1') then
---            if s_ready_s = '1' then 
---                fifo_data_in <= s_last_i & s_data_i;
---                fifo_wr <= '1';
---            else
---                fifo_data_in <= (others => '0');
---                fifo_wr <= '0';
---            end if;
---        end if;
---    end process;
-    
-    -- accepts incomming data as long as the master wants to write and the fifo is not full
-    s_ready_s <= s_valid_i and not fifo_full;
-    s_ready_o <= '1' when s_ready_s = '1' and state_handshake = WAIT_IN_PACKET else '0';
+  
+    -- accepts incomming flit if there is something new and if the FSM is in a receiving state
+    s_ready_o <= '1' when s_valid_i = '1' and (state = WAIT_IN_HEADER or state = GET_SIZE  or state = GET_SOURCE) else
+                -- accepts the incomming flit if there is a new flit and the output port can send data 
+                 '1' when s_valid_i = '1' and m_ready_i = '1' and state = INCREMENTING_PAYLOAD  else '0';
     
     -- get the packet size and the target address for the response packet
     process(clock, reset_n)
     begin
         if (reset_n = '0') then 
-            state_in <= IDLE_IN;
+            state <= WAIT_IN_HEADER;
             source_addr <= (others => '0');
             self_addr <= (others => '0');
             size <= (others => '0');
-            --fifo_wr <= '0';
         elsif (clock'event and clock = '1') then
-            case state_in is
-                -- wait for fifo not empty and discard the header
-                when IDLE_IN =>
-                    --fifo_wr <= '0';
-                    -- start receiving a new packet only when the output packet is sent
-                    if s_ready_s = '1' and state_handshake = WAIT_IN_PACKET then
-                        state_in <= GET_SIZE;
+            case state is
+                -- get the source of the incomming packet , which will be the target of the outgoing packet
+                when WAIT_IN_HEADER =>
+                    if s_valid_i = '1' then
+                        state <= GET_SIZE;
                         self_addr <= s_data_i(15 downto 0);
-                    --    fifo_wr <= '1';
-                    --else
-                    --    fifo_wr <= '0';
                     end if; 
-                -- the fifo has one clock cycle of latency 
-                --when READ_SIZE =>
-                --    state_in <= GET_SIZE;
                 -- the outgoing packet will have the same size of the incomming packet. So, its necessary to save it 
                 when GET_SIZE =>
-                    --fifo_wr <= '0';
-                    if s_ready_s = '1' then
-                        state_in <= GET_STORE_SOURCE;
+                    if s_valid_i = '1' then
+                        state <= GET_SOURCE;
                         size <= s_data_i(15 downto 0);
-                    --    fifo_wr <= '1';
-                    --else
-                    --    fifo_wr <= '0';
                     end if; 
-                -- it's assuming that the first payload flit will tell the source IP address
+                -- it's assuming that the first payload flit will tell the source noc address
                 -- so its possible to send the packet back to it
-                when GET_STORE_SOURCE =>
-                    --fifo_wr <= '0';
-                    if s_ready_s = '1' then
-                        state_in <= WAIT_LAST_FLIT;
+                when GET_SOURCE =>
+                    if s_valid_i = '1' then
+                        state <= SEND_HEADER;
                         source_addr <= s_data_i(15 downto 0);
-                    --    fifo_wr <= '1';
-                    --else
-                    --    fifo_wr <= '0';
                     end if; 
-                when WAIT_LAST_FLIT =>
-                    if s_ready_s = '1' then
-                        --fifo_wr <= '1';
-                        if s_last_i = '1' then
-                            state_in <= IDLE_IN;
+                -- send the header of the outgoing packet if there is no network contention
+                when SEND_HEADER =>
+                    if m_ready_i = '1' then
+                        state <= SEND_SIZE;
+                    end if; 
+                -- send the size of the outgoing packet if there is no network contention
+                when SEND_SIZE =>
+                    if m_ready_i = '1' then
+                        state <= SEND_SOURCE;
+                    end if; 
+                -- send the header of the outgoing packet. just to avoid changing the packet size
+                when SEND_SOURCE =>
+                    if m_ready_i = '1' then
+                        state <= INCREMENTING_PAYLOAD;
+                    end if; 
+                -- increment the rest of the payload
+                when INCREMENTING_PAYLOAD =>
+                    if s_valid_i = '1' and  m_ready_i = '1' then
+                        if  s_last_i = '1' then
+                            state <= WAIT_IN_HEADER;
                         else
-                            state_in <= WAIT_LAST_FLIT;
+                            state <= INCREMENTING_PAYLOAD;
                         end if;
-                        --source_addr <= fifo_data_in(15 downto 0);
-                    --else
-                    --    fifo_wr <= '0';
-                    end if; 
-                -- check if the last signal is high
-                --when WAIT_LAST_FLIT =>
-                --    if s_ready_s = '1'then
-                --        fifo_wr <= '1';
-                --        if s_last_i = '1' then
-                --           state_in <= IDLE_IN;
-                --        end if; 
-                --    else
-                --        fifo_wr <= '0';
-                --    end if; 
+                    else
+                        state <= INCREMENTING_PAYLOAD;
+                    end if;                    
             end case;
         end if; 
     end process;    
 
---    fifo_data_in <= '0' & x"0000" & s_data_i(15 downto 0) when state_in = GET_STORE_SOURCE and s_ready_s = '1' else
---                    '0' & x"0000" & size                  when state_in = STORE_SIZE and s_ready_s = '1' else
---                    s_last_i & s_data_i;
-
-    fifo_data_in <= s_last_i & s_data_i when state_in = WAIT_LAST_FLIT and s_ready_s = '1' else
-                    (others => '0');
-
-    fifo_wr <= '1' when  state_in = WAIT_LAST_FLIT and s_ready_s = '1' else '0';
-    
-    process(clock, reset_n)
-    begin
-        if (reset_n = '0') then 
-            state_handshake <= WAIT_IN_PACKET;
-            can_send <= '0';
-        elsif (clock'event and clock = '1') then
-            case state_handshake is
-                -- wait for fifo not empty and discard the header
-                when WAIT_IN_PACKET =>
-                    if state_in = WAIT_LAST_FLIT then 
-                        can_send <= '1';
-                        -- now that it finished to receive the 1st 3 flits, I can start sending the packet
-                        state_handshake <= WAIT_OUT_PACKET;
-                    else 
-                        can_send <= '0';
-                    end if;
-                when WAIT_OUT_PACKET =>
-                    if state_out = INCREMENTING then 
-                        can_send <= '0';
-                        -- now that it finished to send the 1st 3 flits, It is ready to receive another packet
-                        state_handshake <= WAIT_IN_PACKET;
-                    else 
-                        can_send <= '1';
-                    end if;
-            end case;
-        end if; 
-    end process; 
-
-    ----------------------------------------
-    -- OUTPUT PACKET PROCESSING
-    ----------------------------------------
-    process(clock, reset_n)
-    begin
-        if (reset_n = '0') then 
-            state_out <= IDLE_OUT;
-            fifo_rd <= '0';
-        elsif (clock'event and clock = '1') then
-            case state_out is
-                -- wait for fifo not empty and discard the header
-                when IDLE_OUT =>
-                    fifo_rd <= '0';
-                    -- start to send after the 3 first flits were received
-                    if  m_ready_i = '1' and state_handshake = WAIT_OUT_PACKET then
-                        state_out <= SEND_HEADER;
-                    --    fifo_rd <= '1';
-                    --else
-                    --    fifo_rd <= '0';
-                    end if; 
-                -- send the header of the outgoing packet if there is no network contention
-                when SEND_HEADER =>
-                    fifo_rd <= '0';
-                    if m_ready_i = '1' then
-                        state_out <= SEND_SIZE;
-                    --    fifo_rd <= '1';
-                    --else
-                    --   fifo_rd <= '0';
-                    end if; 
-                -- send the size of the outgoing packet if there is no network contention
-                when SEND_SIZE =>
-                    fifo_rd <= '0';
-                    if m_ready_i = '1' then
-                        state_out <= SEND_SOURCE;
-                    --    fifo_rd <= '1';
-                    --else
-                    --    fifo_rd <= '0';
-                    end if; 
-                -- send the header of the outgoing packet. just to avoid changing the packet size
-                when SEND_SOURCE =>
-                    fifo_rd <= '0';
-                    if m_ready_i = '1' then
-                        state_out <= INCREMENTING;
-                    end if; 
-                -- increment the rest of the payload
-                when INCREMENTING =>
-                    if fifo_empty = '0' and  m_ready_i = '1' then
-                        fifo_rd <= '1';
-                        if  fifo_data_out(32) = '1' then
-                            --fifo_rd <= '0';
-                            state_out <= IDLE_OUT;
-                        else
-                            state_out <= INCREMENTING;
-                        end if;
-                    else
-                        fifo_rd <= '0';
-                        state_out <= INCREMENTING;
-                    end if;
-            end case;
-        end if; 
-	end process;
-
-
     -- send the last position of the FIFOs
-    m_valid_o <= '1' when state_handshake = WAIT_OUT_PACKET and m_ready_i = '1' else '0';
+    m_valid_o <= '1' when m_ready_i = '1' and (state = SEND_HEADER or state = SEND_SIZE or state = SEND_SOURCE) else 
+                 '1' when s_valid_i = '1' and m_ready_i = '1' and state = INCREMENTING_PAYLOAD  else '0';
+                 
     -- it only read from the fifo when the noc can receive a new flit
-    --m_valid_o <= fifo_rd;
-    m_last_o <= '1' when state_out =  INCREMENTING and fifo_data_out(32) = '1' and m_ready_i = '1' else '0';
-    m_data_o <= x"0000" & source_addr when state_out = SEND_HEADER  else
-                x"0000" & size        when state_out = SEND_SIZE    else
-                x"0000" & self_addr   when state_out = SEND_SOURCE  else
-                fifo_data_out(31 downto 0) + INC_VALUE when state_out = INCREMENTING else
+    m_last_o <= '1' when s_valid_i = '1' and m_ready_i = '1' and state = INCREMENTING_PAYLOAD and s_last_i = '1' else '0';
+    m_data_o <= x"0000" & source_addr when state = SEND_HEADER  else
+                x"0000" & size        when state = SEND_SIZE    else
+                x"0000" & self_addr   when state = SEND_SOURCE  else
+                s_data_i + INC_VALUE  when state = INCREMENTING_PAYLOAD else
                 (others => '0');
 
 end Behavioral;
